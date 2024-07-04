0e4732055a2d18c51c3316b96efd5d345fe8c0d6
none <alexander.podkidyshev@gmail.com>
2024-07-04 19:41:40 -0400

    Added flow for last name and refactored

--- content.js
+++ content.js
@@ -1,52 +1,56 @@
 document.addEventListener('focusin', (event) => {
-  if (event.target.tagName === 'INPUT' && 
-    (
-      event.target.id === 'first_name' || 
-      event.target.id === 'firstName' ||
-      event.target.name === 'name' ||
-      event.target.name === 'firstName' ||
-      event.target.getAttribute('data-input') === 'first_name'
-    )) {
-    const inputElement = event.target;
-    
-    // Получаем сохраненное значение из chrome.storage
-    chrome.storage.sync.get(['firstName'], (data) => {
-      let suggestion = data.firstName || '';
+  const inputElement = event.target;
 
-      if (suggestion) {
-        showSuggestionPopup(inputElement, suggestion);
-      }
-    });
-  }
+  if (inputElement.tagName === 'INPUT') {
+    const inputId = inputElement.id;
+    const inputName = inputElement.name;
+    const inputType = inputElement.type;
+    const inputDataInput = inputElement.getAttribute('data-input');
 
-  if (event.target.tagName === 'INPUT' && 
-    (
-      event.target.id === 'email' || 
-      event.target.name === 'email' ||
-      event.target.type === 'email' ||
-      event.target.getAttribute('data-input') === 'email'
-    )) {
-    const inputElement = event.target;
-    
-    // Получаем сохраненное значение из chrome.storage
-    chrome.storage.sync.get(['email'], (data) => {
-      let suggestion = data.email || '';
+    if (
+      (inputId === 'first_name' || inputId === 'firstName' || inputName === 'name' || inputName === 'firstName' || inputDataInput === 'first_name') &&
+      chrome.storage.sync.get(['firstName'], (data) => {
+        const suggestion = data.firstName || '';
+        if (suggestion) {
+          showSuggestionPopup(inputElement, suggestion);
+        }
+      })
+    ) {
+      return;
+    }
 
-      if (suggestion) {
-        showSuggestionPopup(inputElement, suggestion);
-      }
-    });
+    if (
+      (inputId === 'last_name' || inputId === 'lastName' || inputName === 'last_name' || inputName === 'lastName' || inputDataInput === 'last_name') &&
+      chrome.storage.sync.get(['lastName'], (data) => {
+        const suggestion = data.lastName || '';
+        if (suggestion) {
+          showSuggestionPopup(inputElement, suggestion);
+        }
+      })
+    ) {
+      return;
+    }
+
+    if (
+      (inputId === 'email' || inputName === 'email' || inputType === 'email' || inputDataInput === 'email') &&
+      chrome.storage.sync.get(['email'], (data) => {
+        const suggestion = data.email || '';
+        if (suggestion) {
+          showSuggestionPopup(inputElement, suggestion);
+        }
+      })
+    ) {
+      return;
+    }
   }
 });
 
 function showSuggestionPopup(inputElement, suggestion) {
-  // Удаляем старый тултип, если он существует
   const oldTooltip = document.querySelector('.suggestion-popup');
   if (oldTooltip) {
     oldTooltip.remove();
   }
 
-  // Создаем новый тултип
   const popup = document.createElement('div');
   popup.className = 'suggestion-popup';
   popup.innerText = `Insert "${suggestion}"`;
@@ -56,21 +60,19 @@ function showSuggestionPopup(inputElement, suggestion) {
   popup.style.padding = '5px';
   popup.style.cursor = 'pointer';
   popup.style.zIndex = 1000;
-  
+
   const rect = inputElement.getBoundingClientRect();
   popup.style.top = `${window.scrollY + rect.top - 30}px`;
   popup.style.left = `${window.scrollX + rect.left}px`;
 
   document.body.appendChild(popup);
 
-  // Обработчик для вставки значения при клике на тултип
   popup.addEventListener('mousedown', (event) => {
-    event.preventDefault(); // Предотвращаем потерю фокуса на поле ввода
+    event.preventDefault();
     inputElement.value = suggestion;
     popup.remove();
   });
 
-  // Удаление тултипа при потере фокуса
   inputElement.addEventListener('blur', () => {
     if (document.body.contains(popup)) {
       popup.remove();
